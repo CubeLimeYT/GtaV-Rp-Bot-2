0880d9dd8bcba04801397e0c48a9724450fce915
CubeLimeYT <42870582+CubeLimeYT@users.noreply.github.com>
2018-11-01 07:29:39 -1200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const prefix = "//";
+const client = new Discord.Client();
+
+//-----------------------------------------------------------
+
+
+client.login(process.env.BOT_TOKEN);
